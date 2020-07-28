58bf7c788603534d9b95aef32886d3cac2f1fd61
DurgaNandanMishra <durganandan5@gmail.com>
2020-07-28 19:13:57 +0530

    my first git prog

---
+++ Modulus.java
@@ -0,0 +1,16 @@
+class Modulus 
+{
+	public static void main(String[] args) 
+	{
+		int x = 15;
+		if (x%2 == 0)
+		{
+			System.out.println ("its even");
+		}
+		else
+		{
+			System.out.println("its odd");
+		}
+		
+	}
+}
